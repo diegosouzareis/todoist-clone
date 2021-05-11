f8a73ecaeb11b4d629d6d93d7f50e159ac3cf669
DiegoReis1 <fadsky12@gmail.com>
2021-05-10 21:14:56 -0300

    Main design

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 const loading = document.querySelector('.loading-app');
+const buttonAdd = document.querySelector('.add-tasks-controller-button');
+const formTask = document.querySelector('.tasks-form');
+const cancelButton = document.querySelector('.cancel-task-button');
 
 setTimeout(function() {
     loading.classList.add('fade-out');
@@ -7,3 +10,13 @@ setTimeout(function() {
 setTimeout(function() {
     loading.setAttribute('style','display:none');
 }, 1200);
+
+buttonAdd.addEventListener('click', function() {
+    buttonAdd.setAttribute('style','display:none;');
+    formTask.setAttribute('style','display:block;');
+});
+
+cancelButton.addEventListener('click', function() {
+    buttonAdd.setAttribute('style','display:flex;');
+    formTask.setAttribute('style','display:none;');
+});
